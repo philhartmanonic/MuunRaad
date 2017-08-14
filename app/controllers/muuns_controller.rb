c02ce15a67a27378b678b7acd8296b7aeae24199
philhartmanonic <galen.w.burghardt@gmail.com>
2017-08-13 23:52:43 -0500

    Changed the structure so that it goes to the show page, and thus the user can refresh without worry of losing params.

--- app/controllers/muuns_controller.rb
+++ app/controllers/muuns_controller.rb
@@ -10,11 +10,16 @@ class MuunsController < ApplicationController
 	def home
 		@moon = Muun.moon_for_date(Date.today.strftime('%m/%d/%Y'))
 		@muun = Muun.muun_for_date(Date.today.strftime('%m/%d/%Y'))
+		@muuns = Muun.all
+	end
+
+	def show
+		@muun = Muun.find(params[:id])
 	end
 
 	def date
 		date = "#{params[:month]}/#{params[:day]}/#{params[:year]}"
-		@moon = Muun.moon_for_date(date)
 		@muun = Muun.muun_for_date(date)
+		redirect_to action: "show", id: @muun.id
 	end
 end
